4dd2f10b8c05b121a3091d018119484e0b0d5fda
Ross Wintle <rosswintle@users.noreply.github.com>
2022-04-21 16:26:56 +0100

    WIP

---
+++ features.js
@@ -0,0 +1,50 @@
+const features = [
+    {
+        name: 'Enums',
+        description: 'Enums are a way to define a set of named constants.',
+        keywords: [
+            'constants', 'class', 'classes'
+        ],
+        added: '8.1',
+        deprecated: null,
+        removed: null,
+        resources: [
+            {
+                name: 'Enums',
+                url: 'https://stitcher.io/blog/php-enums'
+            }
+        ]
+    },
+    {
+        name: 'Array unpacking with string keys',
+        description: 'e.g. $result = [\'a\' => 0, …$arrayA, …$arrayB];',
+        keywords: [
+            'spread', 'unpacking', 'arrays', 'deconstruct'
+        ],
+        added: '8.1',
+        deprecated: null,
+        removed: null,
+        resources: [
+            {
+                name: 'Enums',
+                url: 'https://stitcher.io/blog/php-enums'
+            }
+        ]
+    },
+    {
+        name: 'Array unpacking (numeric keys only)',
+        description: 'e.g. $result = [0, …$arrayA, …$arrayB, 6, 7];',
+        keywords: [
+            'spread', 'unpacking', 'arrays', 'deconstruct'
+        ],
+        added: '7.4',
+        deprecated: null,
+        removed: null,
+        resources: [
+            {
+                name: 'Enums',
+                url: 'https://stitcher.io/blog/php-enums'
+            }
+        ]
+    },
+]
