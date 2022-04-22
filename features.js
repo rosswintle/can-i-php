d3d6a4c6d43968357cf93f1c290000b5f64cf2d8
Ross Wintle <rosswintle@users.noreply.github.com>
2022-04-22 07:18:07 +0100

    Add data

--- features.js
+++ features.js
@@ -32,6 +32,70 @@ const features = [
         ]
     },
     {
+        name: 'Union types',
+        description: 'A union type declaration accepts values of multiple different simple types, rather than a single one.',
+        keywords: [
+            'types', 'typehints', 'declarations'
+        ],
+        added: '8.0',
+        deprecated: null,
+        removed: null,
+        resources: [
+            {
+                name: 'Union types (php.net)',
+                url: 'https://www.php.net/manual/en/language.types.declarations.php#language.types.declarations.union'
+            }
+        ]
+    },
+    {
+        name: 'Constructor promotion',
+        description: 'Constructor parameters may be promoted to correspond to an object property.',
+        keywords: [
+            'parameters', 'properties', 'classes', 'promoted'
+        ],
+        added: '8.0',
+        deprecated: null,
+        removed: null,
+        resources: [
+            {
+                name: 'Constructor promotion (php.net)',
+                url: 'https://www.php.net/manual/en/language.oop5.decon.php#language.oop5.decon.constructor.promotion'
+            }
+        ]
+    },
+    {
+        name: 'match expression',
+        description: '`match` is like `switch`, but returns a value.',
+        keywords: [
+            'control', 'structures', 'switch', 'conditionals'
+        ],
+        added: '8.0',
+        deprecated: null,
+        removed: null,
+        resources: [
+            {
+                name: 'Match expression (php.net)',
+                url: 'https://www.php.net/manual/en/control-structures.match.php'
+            }
+        ]
+    },
+    {
+        name: 'Typed properties',
+        description: 'Class properties now support type declarations.',
+        keywords: [
+            'properties', 'class', 'types', 'typehints'
+        ],
+        added: '7.4',
+        deprecated: null,
+        removed: null,
+        resources: [
+            {
+                name: 'Typed properties in PHP 7.4 (stitcher.io)',
+                url: 'https://stitcher.io/blog/typed-properties-in-php-74'
+            }
+        ]
+    },
+    {
         name: 'Array unpacking (numeric keys only)',
         description: 'e.g. <code>$result = [0, …$arrayA, …$arrayB, 6, 7];</code>',
         keywords: [
@@ -44,22 +108,6 @@ const features = [
             {
                 name: 'Array unpacking',
                 url: ''
-            }
-        ]
-    },
-    {
-        name: 'Shorthand Ternary (?:)',
-        description: 'Use of <code>?:</code> - actually added in PHP v5.3!',
-        keywords: [
-            'short', 'shorthand', 'ternary', 'conditional', 'operators'
-        ],
-        added: '5.6',
-        deprecated: null,
-        removed: null,
-        resources: [
-            {
-                name: 'Shorthand ternary (stitcher.io)',
-                url: 'https://stitcher.io/blog/shorthand-comparisons-in-php#shorthand-ternary-operator'
             }
         ]
     },
@@ -95,4 +143,20 @@ const features = [
             }
         ]
     },
+    {
+        name: 'Shorthand Ternary (?:)',
+        description: 'Use of <code>?:</code> - actually added in PHP v5.3!',
+        keywords: [
+            'short', 'shorthand', 'ternary', 'conditional', 'operators'
+        ],
+        added: '5.6',
+        deprecated: null,
+        removed: null,
+        resources: [
+            {
+                name: 'Shorthand ternary (stitcher.io)',
+                url: 'https://stitcher.io/blog/shorthand-comparisons-in-php#shorthand-ternary-operator'
+            }
+        ]
+    }
 ]
