79b7910d90c535eabfb3b10e5af5196702e2c56e
Ross Wintle <rosswintle@users.noreply.github.com>
2022-04-22 08:33:06 +0100

    More data

--- features.js
+++ features.js
@@ -32,6 +32,70 @@ const features = [
         ]
     },
     {
+        name: 'Readonly properties',
+        description: 'A property can be declared with the readonly modifier, which prevents modification of the property after initialization.',
+        keywords: [
+            'objects', 'classes', 'properties', 'modifier'
+        ],
+        added: '8.1',
+        deprecated: null,
+        removed: null,
+        resources: [
+            {
+                name: 'Readonly properties (php.net)',
+                url: 'https://www.php.net/manual/en/language.oop5.properties.php#language.oop5.properties.readonly-properties'
+            }
+        ]
+    },
+    {
+        name: 'never type',
+        description: '`never` is a return type indicating the function does not return.',
+        keywords: [
+            'types', 'typehints', 'return'
+        ],
+        added: '8.1',
+        deprecated: null,
+        removed: null,
+        resources: [
+            {
+                name: 'Readonly properties (php.net)',
+                url: 'https://www.php.net/manual/en/language.types.declarations.php#language.types.declarations.never'
+            }
+        ]
+    },
+    {
+        name: 'new in initializers',
+        description: 'Use `new` expressions inside parameter default values, attribute arguments, static variable initializers and global constant initializers.',
+        keywords: [
+            'new', 'objects'
+        ],
+        added: '8.1',
+        deprecated: null,
+        removed: null,
+        resources: [
+            {
+                name: 'new in initializers (stitcher.io)',
+                url: 'https://stitcher.io/blog/php-81-new-in-initializers'
+            }
+        ]
+    },
+    {
+        name: 'First-class callable syntax ',
+        description: 'The first class callable syntax is introduced as of PHP 8.1.0, as a way of creating anonymous functions from callable.',
+        keywords: [
+            'closures'
+        ],
+        added: '8.1',
+        deprecated: null,
+        removed: null,
+        resources: [
+            {
+                name: 'First class callable syntax (php.net)',
+                url: 'https://www.php.net/manual/en/functions.first_class_callable_syntax.php'
+            }
+        ]
+    },
+    {
         name: 'Union types',
         description: 'A union type declaration accepts values of multiple different simple types, rather than a single one.',
         keywords: [
@@ -80,6 +144,86 @@ const features = [
         ]
     },
     {
+        name: 'Named arguments',
+        description: 'Named arguments allow passing arguments to a function based on the parameter name, rather than the parameter position.',
+        keywords: [
+            'parameters', 'call', 'functions'
+        ],
+        added: '8.0',
+        deprecated: null,
+        removed: null,
+        resources: [
+            {
+                name: 'Named arguments (php.net)',
+                url: 'https://www.php.net/manual/en/functions.arguments.php#functions.named-arguments'
+            }
+        ]
+    },
+    {
+        name: 'Nullsafe operator',
+        description: 'Nullsafe operator allows you to access properties and methods of nullable objects. e.g. `$country = $session?->user?->getAddress()?->country;`',
+        keywords: [
+            'null'
+        ],
+        added: '8.0',
+        deprecated: null,
+        removed: null,
+        resources: [
+            {
+                name: 'Nullsafe operator (stitcher.io)',
+                url: 'https://stitcher.io/blog/php-8-nullsafe-operator'
+            }
+        ]
+    },
+    {
+        name: 'str_contains',
+        description: 'Determine if a string contains a given substring',
+        keywords: [
+            'functions', 'helpers', 'strings', 'utilities', 'utils'
+        ],
+        added: '8.0',
+        deprecated: null,
+        removed: null,
+        resources: [
+            {
+                name: 'str_contains (php.net)',
+                url: 'https://www.php.net/manual/en/function.str-contains.php'
+            }
+        ]
+    },
+    {
+        name: 'str_starts_with',
+        description: 'Check if a string starts with a given substring',
+        keywords: [
+            'functions', 'helpers', 'strings', 'utilities', 'utils'
+        ],
+        added: '8.0',
+        deprecated: null,
+        removed: null,
+        resources: [
+            {
+                name: 'str_starts_with (php.net)',
+                url: 'https://www.php.net/manual/en/function.str-starts-with.php'
+            }
+        ]
+    },
+    {
+        name: 'str_ends_with',
+        description: 'Check if a string ends with a given substring',
+        keywords: [
+            'functions', 'helpers', 'strings', 'utilities', 'utils'
+        ],
+        added: '8.0',
+        deprecated: null,
+        removed: null,
+        resources: [
+            {
+                name: 'str_ends_with (php.net)',
+                url: 'https://www.php.net/manual/en/function.str-ends-with.php'
+            }
+        ]
+    },
+    {
         name: 'Typed properties',
         description: 'Class properties now support type declarations.',
         keywords: [
@@ -92,6 +236,22 @@ const features = [
             {
                 name: 'Typed properties in PHP 7.4 (stitcher.io)',
                 url: 'https://stitcher.io/blog/typed-properties-in-php-74'
+            }
+        ]
+    },
+    {
+        name: 'Arrow functions',
+        description: 'Arrow functions are a shorthand for anonymous functions: `fn (argument_list) => expr`.',
+        keywords: [
+            'closures', 'anonymous', 'shorthand', 'short'
+        ],
+        added: '7.4',
+        deprecated: null,
+        removed: null,
+        resources: [
+            {
+                name: 'Arrow functions (php.net)',
+                url: 'https://www.php.net/manual/en/functions.arrow.php'
             }
         ]
     },
@@ -112,6 +272,70 @@ const features = [
         ]
     },
     {
+        name: 'Multi catch exception handline',
+        description: 'A catch block may specify multiple exceptions using the pipe (|) character',
+        keywords: [
+            'exceptions'
+        ],
+        added: '7.1',
+        deprecated: null,
+        removed: null,
+        resources: [
+            {
+                name: 'Exceptions (php.net)',
+                url: 'https://www.php.net/manual/en/language.exceptions.php'
+            }
+        ]
+    },
+    {
+        name: 'nullable type declarations',
+        description: 'Type declarations can be marked nullable by prefixing the type name with a question mark (?). This signifies that the value can be of the specified type or null.',
+        keywords: [
+            'declarations', 'typehints', 'return'
+        ],
+        added: '7.1',
+        deprecated: null,
+        removed: null,
+        resources: [
+            {
+                name: 'Nullable type (php.net)',
+                url: 'https://www.php.net/manual/en/language.types.declarations.php#language.types.declarations.nullable'
+            }
+        ]
+    },
+    {
+        name: 'void return type',
+        description: '<code>void</code> is a return type indicating the function does not return a value.',
+        keywords: [
+            'declarations', 'typehints', 'return'
+        ],
+        added: '7.1',
+        deprecated: null,
+        removed: null,
+        resources: [
+            {
+                name: 'void return type (php.net)',
+                url: 'https://www.php.net/manual/en/language.types.declarations.php#language.types.declarations.return-only'
+            }
+        ]
+    },
+    {
+        name: 'Group use declarations',
+        description: 'Classes, functions and constants being imported from the same namespace can be grouped together in a single use statement.',
+        keywords: [
+            'imports'
+        ],
+        added: '7.0',
+        deprecated: null,
+        removed: null,
+        resources: [
+            {
+                name: 'Group use declarations (php.net)',
+                url: 'https://www.php.net/manual/en/language.namespaces.importing.php#language.namespaces.importing.group'
+            }
+        ]
+    },
+    {
         name: 'Null coalescing operator',
         description: 'Use <code>??</code>',
         keywords: [
@@ -124,6 +348,22 @@ const features = [
             {
                 name: 'Null coalescing operator (php.net)',
                 url: 'https://www.php.net/manual/en/language.operators.comparison.php#language.operators.comparison.coalesce'
+            }
+        ]
+    },
+    {
+        name: 'Return type declarations',
+        description: 'Return type declarations specify the type of the value that will be returned from a function',
+        keywords: [
+            'typehints', 'types'
+        ],
+        added: '7.0',
+        deprecated: null,
+        removed: null,
+        resources: [
+            {
+                name: 'Return type declarations (php.net)',
+                url: 'https://www.php.net/manual/en/migration70.new-features.php#migration70.new-features.return-type-declarations'
             }
         ]
     },
