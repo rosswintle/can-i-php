39a54dafabe3294a7d8f26cea9415f45e9e7cacf
Ross Wintle <rosswintle@users.noreply.github.com>
2025-12-07 18:34:09 +0000

    Add cache-busting prod script

--- watch.sh
+++ watch.sh
@@ -1,2 +1,10 @@
+echo "Removing CSS cache ID"
+
+rm dist/style-*.css
+# Notes
+# 1. The -i flag requires a parameter on MacOS
+# 2. This only works on index.html for this project
+sed -E -i '' "s#dist/style(\\-[0-9]+)?\\.css#dist/style.css#g" index.html
+
 #!/bin/sh
-npx tailwindcss -i ./src/style.css -o ./dist/style.css --watch
+npx @tailwindcss/cli -i ./src/style.css -o ./dist/style.css --watch
