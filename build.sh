39a342c97382a4c1b38a5d7d827fcd3c31c02c37
Ross Wintle <rosswintle@users.noreply.github.com>
2025-12-07 19:37:32 +0000

    Cache-bust all asset files

--- build.sh
+++ build.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 npx @tailwindcss/cli -i ./src/style.css -o ./dist/style.css
 
-echo "Removing CSS cache ID"
-
-rm dist/style-*.css
-# Notes
-# 1. The -i flag requires a parameter on MacOS
-# 2. This only works on index.html for this project
-sed -E -i '' "s#dist/style(\\-[0-9]+)?\\.css#dist/style.css#g" index.html
-
 echo "Building JSON file..."
 
 node <<EOF
@@ -17,5 +9,24 @@ const features = require('./features.js');
 jsonData = JSON.stringify(features);
 fs.writeFileSync('./features.json', jsonData);
 EOF
+
+echo "Cache-busting files"
+
+NOW=`date +%s`
+rm dist/style-*.css
+rm dist/features-*.js
+rm dist/features-*.json
+rm dist/versions-*.js
+cp dist/style.css dist/style-$NOW.css
+cp features.js dist/features-$NOW.js
+cp features.json dist/features-$NOW.json
+cp versions.js dist/versions-$NOW.js
+# Notes
+# 1. The -i flag requires a parameter on MacOS
+# 2. This only works on index.html for this project
+sed -E -i '' "s#dist/style(\\-[0-9]+)?\\.css#dist/style-$NOW.css#g" index.html
+# This will catch both the JS and the JSON
+sed -E -i '' "s#dist/features(\\-[0-9]+)?\\.js#dist/features-$NOW.js#g" index.html
+sed -E -i '' "s#dist/versions(\\-[0-9]+)?\\.js#dist/versions-$NOW.js#g" index.html
 
 echo "Done!"
---
+++ dist/versions-1765136123.js
@@ -0,0 +1,79 @@
+/**
+ * See: https://www.php.net/supported-versions.php
+ *
+ * and
+ *
+ * https://en.wikipedia.org/wiki/PHP#Release_history
+ *
+ * and (even better)
+ *
+ * https://endoflife.date/php
+ */
+const versions = [
+  {
+    version: "5.6",
+    releasedDate: "2014-08-28",
+    deprecatedDate: "2017-01-19",
+    eolDate: "2019-01-10",
+  },
+  {
+    version: "7.0",
+    releasedDate: "2015-12-03",
+    deprecatedDate: "2018-01-04",
+    eolDate: "2018-12-31",
+  },
+  {
+    version: "7.1",
+    releasedDate: "2016-12-01",
+    deprecatedDate: "2018-12-01",
+    eolDate: "2019-12-01",
+  },
+  {
+    version: "7.2",
+    releasedDate: "2017-11-30",
+    deprecatedDate: "2019-11-30",
+    eolDate: "2020-11-30",
+  },
+  {
+    version: "7.3",
+    releasedDate: "2018-12-06",
+    deprecatedDate: "2020-12-06",
+    eolDate: "2021-12-06",
+  },
+  {
+    version: "7.4",
+    releasedDate: "2019-11-28",
+    deprecatedDate: "2021-11-28",
+    eolDate: "2022-11-28",
+  },
+  {
+    version: "8.0",
+    releasedDate: "2020-11-26",
+    deprecatedDate: "2022-11-26",
+    eolDate: "2023-11-26",
+  },
+  {
+    version: "8.1",
+    releasedDate: "2021-11-25",
+    deprecatedDate: "2023-11-25",
+    eolDate: "2024-11-25",
+  },
+  {
+    version: "8.2",
+    releasedDate: "2022-12-08",
+    deprecatedDate: "2024-12-08",
+    eolDate: "2025-12-08",
+  },
+  {
+    version: "8.3",
+    releasedDate: "2023-11-23",
+    deprecatedDate: "2025-12-31",
+    eolDate: "2027-12-31"
+  },
+  {
+    version: "8.4",
+    releasedDate: "2024-11-21",
+    deprecatedDate: "2026-12-31",
+    eolDate: "2028-12-31"
+  }
+];
